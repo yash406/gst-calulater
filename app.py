285f23d17334101962aed4643917411d2a77b1ac
yash406 <71926816+yash406@users.noreply.github.com>
2025-07-12 06:38:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -150,10 +150,12 @@ gst_rates = {
     'sin goods': 28, # General category for items like tobacco, pan masala
 
     # Additional GST Rates
-    'gold': 3,
+   'gold': 3,
     'precious stones': 3,
-    'diamonds (non-industrial)': 0.25,
-}
+    'jewellery': 3,
+    'Artificial jewellery items':3,
+    'Rough diamonds':0.25,
+    'uncut gems':0.25,
 
 invoices = []
 
